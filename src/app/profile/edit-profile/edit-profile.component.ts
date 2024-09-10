0f976166b7061309f3cab4df0c4ceec6d92e50d0
abdul-jabbar-dev <abdul.jabbar.dev@gmail.com>
2024-09-10 19:59:12 +0600

    working update profile

--- src/app/profile/edit-profile/edit-profile.component.ts
+++ src/app/profile/edit-profile/edit-profile.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { RequestService } from 'src/app/services/request.service';
+
 interface TUser {
-  password?: string;
-  role?: 'admin' | 'subscriber'|'rider';
+  new_password?: string;
+  role?: 'admin' | 'subscriber' | 'rider';
   first_name: string;
   email: string;
   username?: string;
@@ -11,19 +12,27 @@ interface TUser {
   age: number;
   phone: string;
 }
+
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.css'],
 })
 export class EditProfileComponent implements OnInit {
-  profile: (TUser & { img: string }) | null = null;
+  toggoleShow() {
+    this.isVisible = !this.isVisible;
+  }
+  profile: TUser | null = null;
+  isVisible = false;
+
   constructor(public request: RequestService) {}
 
   async ngOnInit(): Promise<void> {
     try {
       await this.getProfile();
-    } catch (error) {}
+    } catch (error) {
+      console.error('Error loading profile:', error);
+    }
   }
 
   async getProfile() {
@@ -31,9 +40,21 @@ export class EditProfileComponent implements OnInit {
       this.profile = await firstValueFrom(
         await this.request.get('/user/get_my_profile')
       );
-      
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  async saveProfile() {
+    try {
+      console.log(this.profile);
+      // Make a PUT request to save the profile
+      const response = await firstValueFrom(
+        await this.request.put('/user/update_profile', this.profile)
+      );
+      console.log('Profile updated successfully:', response);
+    } catch (error) {
+      console.error('Error updating profile:', error);
     }
   }
 }
